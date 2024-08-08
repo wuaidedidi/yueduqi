544d81b00ab20c70d0018aedec5ec7972b716cc7
wuaidedidi <470357714@qq.com>
2024-08-08 19:33:38 +0800

    chore: 增加一些请求

---
+++ src/api/book.ts
@@ -0,0 +1,24 @@
+// 获取小说章节内容的 API
+import { http } from '@/utils/http'
+
+// 获取章节列表的 API 调用
+export const getChaptersAPI = async () => {
+  return http<{ id: string; title: string }[]>({
+    url: '/api/novel/chapters',
+    method: 'GET',
+  })
+}
+
+// 获取章节内容的 API 调用
+export const getChapterContentAPI = async (fileName: string, chapterId: string, page: number, pageSize: number) => {
+  return http<string>({
+    url: `/api/novel/chapter`,
+    method: 'POST',
+    data: {
+      fileName,
+      chapterId,
+      page,
+      pageSize,
+    },
+  })
+}
